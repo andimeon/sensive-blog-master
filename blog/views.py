5f4608a0fdb53c44ab655e7e5beb6cc173ef2219
andimeon <andimeon@gmail.com>
2020-06-16 16:56:24 +0900

    Replace sorted func with order_by

--- blog/views.py
+++ blog/views.py
@@ -28,13 +28,9 @@ def serialize_tag(tag):
     }
 
 
-def get_likes_count(post):
-    return post.likes__count
-
-
 def index(request):
     posts = Post.objects.annotate(Count('likes'))
-    most_popular_posts = sorted(posts, key=get_likes_count, reverse=True)[:5]
+    most_popular_posts = posts.order_by('-likes__count')[:5]
     
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
