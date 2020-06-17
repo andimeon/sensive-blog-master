ede91d7e34eb7aa2b76d532798f55d4b90b10a77
andimeon <andimeon@gmail.com>
2020-06-17 21:38:12 +0900

    Move queries into QuerySet methods

--- blog/models.py
+++ blog/models.py
@@ -11,23 +11,42 @@ class TagQuerySet(models.QuerySet):
         return popular_tags
 
 
+class PostQuerySet(models.QuerySet):
+    
+    def popular(self):
+        popular_post = self.annotate(Count('likes')).order_by('-likes__count')
+        return popular_post
+    
+    def fetch_with_comments_count(self):
+        posts_ids = [post.id for post in self]
+        posts_with_comments = Post.objects.filter(id__in=posts_ids).annotate(comments__count=Count('comments'))
+        ids_and_comments = posts_with_comments.values_list('id', 'comments__count')
+        count_for_id = dict(ids_and_comments)
+        for post in self:
+            post.comments__count = count_for_id[post.id]
+        return self
+
+
 class Post(models.Model):
     title = models.CharField("Заголовок", max_length=200)
     text = models.TextField("Текст")
     slug = models.SlugField("Название в виде url", max_length=200)
     image = models.ImageField("Картинка")
     published_at = models.DateTimeField("Дата и время публикации")
-
-    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор", limit_choices_to={'is_staff': True})
+    
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор",
+                               limit_choices_to={'is_staff': True})
     likes = models.ManyToManyField(User, related_name="liked_posts", verbose_name="Кто лайкнул", blank=True)
     tags = models.ManyToManyField("Tag", related_name="posts", verbose_name="Теги")
-
+    
+    objects = PostQuerySet.as_manager()
+    
     def __str__(self):
         return self.title
-
+    
     def get_absolute_url(self):
         return reverse('post_detail', args={'slug': self.slug})
-
+    
     class Meta:
         ordering = ['-published_at']
         verbose_name = 'пост'
@@ -36,18 +55,18 @@ class Post(models.Model):
 
 class Tag(models.Model):
     title = models.CharField("Тег", max_length=20, unique=True)
-
+    
     objects = TagQuerySet.as_manager()
-
+    
     def __str__(self):
         return self.title
-
+    
     def clean(self):
         self.title = self.title.lower()
-
+    
     def get_absolute_url(self):
         return reverse('tag_filter', args={'tag_title': self.slug})
-
+    
     class Meta:
         ordering = ["title"]
         verbose_name = 'тег'
@@ -58,13 +77,13 @@ class Comment(models.Model):
     post = models.ForeignKey("Post", on_delete=models.CASCADE, verbose_name="Пост, к которому написан",
                              related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор", related_name='comments')
-
+    
     text = models.TextField("Текст комментария")
     published_at = models.DateTimeField("Дата и время публикации")
-
+    
     def __str__(self):
         return f"{self.author.username} under {self.post.title}"
-
+    
     class Meta:
         ordering = ['published_at']
         verbose_name = 'комментарий'
