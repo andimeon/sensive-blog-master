65e6d1bf1564fc2d51abc02af452eb5d70c4c9f4
andimeon <andimeon@gmail.com>
2020-07-10 23:13:58 +0900

    handled object DoesNotExist

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Comment, Post, Tag
-from django.db.models import Count, Prefetch
 
 
 def serialize_post(post):
+    post_tags = post.tags.all()
     return {
         "title": post.title,
         "teaser_text": post.text[:200],
@@ -12,8 +12,8 @@ def serialize_post(post):
         "image_url": post.image.url if post.image else None,
         "published_at": post.published_at,
         "slug": post.slug,
-        "tags": [serialize_tag(tag) for tag in post.tags.all()],
-        'first_tag_title': post.tags.first().title,
+        "tags": [serialize_tag(tag) for tag in post_tags],
+        'first_tag_title': post_tags[0].title,
     }
 
 
@@ -44,8 +44,8 @@ def index(request):
 def post_detail(request, slug):
     posts = Post.objects.popular(). \
         fetch_with_posts_count()
+    post = get_object_or_404(posts, slug=slug)
     
-    post = posts.get(slug=slug)
     comments = Comment.objects.filter(post=post).prefetch_related('author')
     serialized_comments = []
     for comment in comments:
